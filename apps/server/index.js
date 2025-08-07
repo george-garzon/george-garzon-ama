e7e8ea6b3195b30db68442a2c889511287aa96d4
George Garzon Mac <george.garzon@olympiapharmacy.com>
2025-08-06 22:53:56 -0400

    AI Chatbot - axios

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 // const express = require('express');
 // const cors = require('cors');
-// const axios = require('axios');
+const axios = require('axios');
 // const app = express();
 //
 // const corsOptions = {
